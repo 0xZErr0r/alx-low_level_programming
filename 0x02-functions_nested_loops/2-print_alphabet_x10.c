899d08eeb9143f261ed307968a363626ed689d72
Eyob <99369587+0xZErr0r@users.noreply.github.com>
2022-03-17 04:02:04 -0700

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
-#include "holberton.h"
+#include "main.h"
 /**
- * print_alphabet_x10 - check the code for Holberton School students
+ * print_alphabet_x10 - check the code
  */
 void print_alphabet_x10(void)
 {
