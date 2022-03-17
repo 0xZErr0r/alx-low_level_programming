6c2350c17bdb93975ba505568cd01ed5935e3aec
Eyob <eyobpassportemail@gmail.com>
2022-03-17 03:44:38 -0700

    late push of functions, nested loops

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * main - calling
+ * _putchar: new function
+ * Return: 1 if the number is positive. 0 otherwise
+ */
+int main(void)
+{
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
+return (0);
+}
